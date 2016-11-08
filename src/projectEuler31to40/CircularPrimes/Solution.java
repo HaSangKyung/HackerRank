cd4c48bb8817114d893e386079eef632c6378b6d
HaSangKyung <diwm14@gmail.com>
2016-11-08 14:21:24 +0900

    [2016/11/08] Project Euler #36 : Double-base palindromes

---
+++ src/projectEuler31to40/CircularPrimes/Solution.java
@@ -0,0 +1,14 @@
+package projectEuler31to40.CircularPrimes;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+		Scanner scanner = new Scanner(System.in);
+		
+	
+    }
+	
+}
