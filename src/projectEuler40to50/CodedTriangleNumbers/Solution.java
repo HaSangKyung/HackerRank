5d141ce44f91f3a3c22622a9452e4bc66e6f3462
HaSangKyung <diwm14@gmail.com>
2016-11-10 15:31:22 +0900

    [2016/11/10] Project Euler #42 : Coded triangle numbers

---
+++ src/projectEuler40to50/CodedTriangleNumbers/Solution.java
@@ -0,0 +1,32 @@
+package projectEuler40to50.CodedTriangleNumbers;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	 public static void main(String[] args) {
+	        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+		 Scanner scanner = new Scanner(System.in);
+		 int T = scanner.nextInt();
+		 while(T >= 1){
+			 long tn = scanner.nextLong();
+			 System.out.println(getSolution(tn));
+			 T--;
+		 }
+		 
+    }
+	 
+	 public static long getSolution(long tn){
+		 long c = 2*tn;
+		 long valueInRoot = 1 + 4*c;
+		 
+		 double root = Math.sqrt(valueInRoot);
+		 
+		 if(root == (long)root && (root - 1) % 2 == 0){
+			 return (long)(root-1)/2;
+		 }
+		 
+		 return -1;
+	 }
+	 
+}
