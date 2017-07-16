0ba625800f9c26843afe168c86a06351754ee254
HaSangKyung <diwm14@gmail.com>
2017-07-16 14:19:50 +0900

    Checkout repository in intellij

---
+++ src/algorithms/EvenTree/Solution.java
@@ -0,0 +1,29 @@
+package algorithms.EvenTree;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution{
+	public static void main(String[] args) {
+		String str = "c";
+		
+		int left = 0;
+		int right = str.length()-1;
+		int result = str.length();
+		System.out.println(right);
+		int count = 0;
+		while(left <= right){
+			if(!str.substring(left, left+1).equals(str.substring(right, right+1))){
+				left++;
+				count++;
+			}else{
+				left++;
+				right--;
+			}
+		}
+		
+		System.out.println(count+result);
+		
+	}
+	
+}
